c6c88dc047b69ad6560c09e129292390b1ff57ae
Kavithasri1994 <skavithasri024@gmail.com>
2022-01-11 20:54:10 +0530

    My Project

---
+++ src/org/sample/Sample.java
@@ -0,0 +1,28 @@
+package org.sample;
+
+public class Sample {
+private void tc1() {
+System.out.println("Test Case1");
+}
+private void tc2() {
+System.out.println("Test Case2");
+}
+private void tc3() {
+	System.out.println("Test Case3");
+	}
+private void tc4() {
+System.out.println("Test Case4");
+}
+private void tc5() {
+	System.out.println("Test Case5");
+	}
+public static void main(String[] args) {
+Sample s = new Sample();
+s.tc1();
+s.tc2();
+s.tc3();
+s.tc4();
+s.tc5();
+}
+
+}
